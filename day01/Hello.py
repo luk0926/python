eca6b6dc8ba4f8e427eb247ba463d4411099eb6e
luk0926 <18514815382@163.com>
2019-02-24 16:46:26 +0800

    add hello.py

---
+++ day01/Hello.py
@@ -0,0 +1,15 @@
+#!/user/bin/python
+# -*- conding:utf-8 -*-
+'''
+@File    :   Hello.py    
+@Contact :   18514815382@163.com
+@License :   (C)Copyright 2017-2018, Liugroup-NLPR-CASIA
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2019/2/24 16:33   luk      1.0         None
+'''
+
+# import lib
+
+print("hello")
